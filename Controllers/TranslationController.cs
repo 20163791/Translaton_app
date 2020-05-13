6090ae12352513cb6df9a4d7a11944be5cd584a2
unknown <ruta.jankeviciute@stud.vgtu.lt>
2020-05-13 06:11:32 +0300

    initial commit

---
+++ Controllers/TranslationController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Mvc;
+using translator.Models;
+
+namespace translator.Controllers
+{
+    public class TranslationController : Controller
+    {
+        // GET: Translation
+        [HttpGet]
+        public JsonResult GetTranslation(String input_text)
+        {
+            Translation translation = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri($"http://127.0.0.1:5000/");
+                var responseTask = client.GetAsync($"translate/{input_text}");
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<Translation>();
+                    readTask.Wait();
+                    translation = readTask.Result;
+                    translation.output_text = Cleanup(translation.output_text);
+                }
+                else
+                {
+                    translation.output_text = "error";
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return Json(translation, JsonRequestBehavior.AllowGet);
+        }
+
+        public String Cleanup(String input)
+        {
+            return input.Substring(0, input.Length - 7);
+        }
+    }
+}
---
+++ Models/Translation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace translator.Models
+{
+    public class Translation
+    {
+        public String output_text { get; set; }
+    }
+}
